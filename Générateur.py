ca1273d9b8f0e271acc05f27d15217a6d2282e27
reza0310 <michelageou@gmail.com>
2022-02-18 16:23:43 +0100

    Upload

---
+++ Générateur.py
@@ -0,0 +1,24 @@
+import time
+import os
+
+def fibo(n):
+    f=1
+    fm1=1
+    fm2=0
+    for i in range(n):
+        f=fm1+fm2;fm2=fm1;fm1=f
+    return f
+
+n=1
+log = open("log.txt", "a", encoding="utf-8")
+while True:
+    n*=10
+    debut = time.time()
+    fibonacci = str(fibo(n))
+    fin = time.time()
+    with open("Fibonacci "+str(n)+".txt", "w", encoding="utf-8") as f:
+        f.write(fibonacci)
+    txt = "Fibonacci de "+str(n)+" effectué en "+str(fin-debut)+" secondes. Nombre de caractères: "+str(len(fibonacci))+".\n"
+    log.seek(os.SEEK_END)
+    log.write(txt)
+    print(txt)
